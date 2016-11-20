2de3fe219e76fd1562c68ded5c8139b581941b60
DESKTOP-47CKMKT\Alexander <at.webdes@gmail.com>
2016-11-19 23:40:19 -0500

    Working on annotations

--- Scripts/controllers/markersController.js
+++ Scripts/controllers/markersController.js
@@ -2,7 +2,7 @@ app.controller('markersController', function ($scope, $http, $window, $mdDialog,
 
     uiGmapGoogleMapApi.then(function (map) {
         getLocation();
-        bindMarkers($scope.position.coords);
+        bindMarkers($scope.location.coords);
     });
 
     $scope.map = {
@@ -32,27 +32,31 @@ app.controller('markersController', function ($scope, $http, $window, $mdDialog,
             };
         }
     }
-    $scope.position = {
+
+    $scope.location = {
         id: 0,
         coords: {
             latitude: 40.778063,
             longitude: -73.970085
         },
-        options: { draggable: true },
+        options: {
+            draggable: true,
+            icon: 'Content/img/location.png'
+        },
         events: markerEvents
     };
+
     $scope.markers = [];
-    
+    var destination = { lat: 0, lng: 0 };
+
     $scope.getDirections = function () {
 
         var directionsDisplay = new google.maps.DirectionsRenderer();
         var directionsService = new google.maps.DirectionsService();
-        directionsDisplay.setMap(null);
-        directionsDisplay.setPanel(null);
-
+       
         $scope.directions = {
-            origin: { lat: $scope.position.coords.latitude, lng: $scope.position.coords.longitude },
-            destination: { lat: 40.780719, lng: -73.986958 },
+            origin: { lat: $scope.location.coords.latitude, lng: $scope.location.coords.longitude },
+            destination: destination,
             showList: false
         }
 
@@ -64,24 +68,22 @@ app.controller('markersController', function ($scope, $http, $window, $mdDialog,
 
         directionsService.route(request, function (response, status) {
             if (status === google.maps.DirectionsStatus.OK) {
-                
                 directionsDisplay.setDirections(response);
                 directionsDisplay.setMap($scope.map.control.getGMap());
                 directionsDisplay.setPanel(document.getElementById('directionsList'));
                 $scope.directions.showList = true;
             } else {
-                Alert($mdDialog, "Error", "Sorry, we can't build its route!");
+                Alert($mdDialog, "Error", "Sorry, we can't build this route!");
             }
         });
     }
-    
 
     function getLocationSuccFn(position) {
 
         $scope.map.center.latitude = position.coords.latitude;
         $scope.map.center.longitude = position.coords.longitude;
-        $scope.position.coords.latitude = position.coords.latitude;
-        $scope.position.coords.longitude = position.coords.longitude;
+        $scope.location.coords.latitude = position.coords.latitude;
+        $scope.location.coords.longitude = position.coords.longitude;
         $scope.map.control.refresh({ latitude: $scope.map.center.latitude, longitude: $scope.map.center.longitude });
     }
 
@@ -108,13 +110,20 @@ app.controller('markersController', function ($scope, $http, $window, $mdDialog,
         }
     }
 
+    $scope.onClick = function (marker, eventName, model) {
+
+        model.show = !model.show;
+        destination.lat = model.latitude;
+        destination.lng = model.longitude;
+    };
+
     function bindMarkers(location) {
         //var markers = getMarkers(location);
         // mock markers
         var markers = [
-                        { id: 0, latitude: 40.783857, longitude: -73.975964 },
-                        { id: 1, latitude: 40.781818, longitude: -73.979322 },
-                        { id: 2, latitude: 40.780719, longitude: -73.986958 }
+                        { id: 0,  latitude: 40.783857, longitude: -73.975964, info: { rating: 2} },
+                        { id: 1,  latitude: 40.781818, longitude: -73.979322, info: { rating: 45} },
+                        { id: 2,  latitude: 40.780719, longitude: -73.986958, info: { rating: 999 } }
                     ];
         if (!isEmpty(markers)) {
             $scope.markers = markers;
@@ -143,4 +152,9 @@ app.controller('markersController', function ($scope, $http, $window, $mdDialog,
 
         });
     }
+
+});
+
+app.controller('InfoCtrl', function ($scope) {
+
 });
